460e574ae543c897fc9ff2ea355739c013796731
luisduenas <luis@octahedroid.com>
2022-04-19 20:11:47 -0700

    feat: add remark options

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,29 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 630,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+          `gatsby-remark-emoji`,
+        ],
+      },
+    },
     `gatsby-plugin-sharp`,
     // {
     //   resolve: `gatsby-plugin-feed`,
